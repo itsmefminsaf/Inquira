106e94a1fe1f3076cd75bba16e02b4eec7af68e0
itsmefminsaf <itsmefminsaf@gmail.com>
2025-05-03 17:59:09 +0530

    fix: add expiration time to session storage in createSession function

--- src/utils/createSession.ts
+++ src/utils/createSession.ts
@@ -6,7 +6,7 @@ const createSession = async (email: string) => {
   try {
     const session_id = crypto.randomBytes(512).toString("hex").normalize();
 
-    redis.set(session_id, email);
+    redis.set(session_id, email, { ex: 60 * 60 * 24 * 7 });
     (await cookies()).set("session_id", session_id);
   } catch (error) {
     console.log("Error creating session: ", error);
